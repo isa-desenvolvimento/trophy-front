aac804e5f7618d20ab085cde7d3f81aafeac8226
Andressa Novaes <isa.menezes@ilia.digital>
2021-02-02 11:50:09 -0300

    style: tF-3 Ajuste layout signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,53 +1,58 @@
 <template>
-  <div class="vue-tempalte">
-    <form @submit.prevent="userRegistration">
-      <h3>Sign Up</h3>
+  <div class="container-2">
+    <card>
+      <form @submit.prevent="userRegistration" class="sign-up">
+        <h3>Sign Up</h3>
 
-      <div class="form-group">
-        <label>Name</label>
-        <input
-          id="inp-signup-name"
-          type="text"
-          class="form-control form-control-lg"
-          v-model="user.name"
-        />
-      </div>
+        <div class="form-group">
+          <label>Name</label>
+          <input
+            id="inp-signup-name"
+            type="text"
+            class="form-control form-control-md input"
+            v-model="user.name"
+          />
+        </div>
 
-      <div class="form-group">
-        <label>Email</label>
-        <input
-          id="inp-signup-email"
-          type="email"
-          class="form-control form-control-lg"
-          v-model="user.email"
-        />
-      </div>
+        <div class="form-group">
+          <label>Email</label>
+          <input
+            id="inp-signup-email"
+            type="email"
+            class="form-control form-control-md input"
+            v-model="user.email"
+          />
+        </div>
 
-      <div class="form-group">
-        <label>Password</label>
-        <input
-          id="inp-signup-password"
-          type="password"
-          class="form-control form-control-lg"
-          v-model="user.password"
-        />
-      </div>
+        <div class="form-group">
+          <label>Password</label>
+          <input
+            id="inp-signup-password"
+            type="password"
+            class="form-control form-control-md input"
+            v-model="user.password"
+          />
+        </div>
 
-      <button type="submit" class="btn btn-dark btn-lg btn-block">
-        Sign Up
-      </button>
+        <button type="submit" class="btn btn-success btn-sm btn-block">
+          Sign Up
+        </button>
 
-      <p class="forgot-password text-right">
-        Already registered
-        <router-link :to="{ name: 'login' }">sign in?</router-link>
-      </p>
-    </form>
+        <p class="forgot-password text-right">
+          Already registered
+          <router-link :to="{ name: 'login' }">sign in?</router-link>
+        </p>
+      </form>
+    </card>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
+import Card from "@/components/Card.vue";
+
 export default {
+  components: { Card },
   data() {
     return {
       user: {
