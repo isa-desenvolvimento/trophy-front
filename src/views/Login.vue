cfd05955f0d3130afa7c0026642235cc4ce9ccb6
Andressa Novaes <isa.menezes@ilia.digital>
2021-02-02 11:56:14 -0300

    style: tF-3 Layout login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,43 +1,47 @@
 <template>
-  <div class="vue-tempalte">
-    <form @submit.prevent="userLogin">
-      <h3>Sign In</h3>
+  <div class="container-2">
+    <card>
+      <form @submit.prevent="userLogin" class="sign-up">
+        <h3>Sign In</h3>
 
-      <div class="form-group">
-        <label>Email address</label>
-        <input
-          id="inp-login-email"
-          type="email"
-          class="form-control form-control-lg"
-          v-model="user.email"
-        />
-      </div>
+        <div class="form-group">
+          <label>Email address</label>
+          <input
+            id="inp-login-email"
+            type="email"
+            class="form-control form-control-md input"
+            v-model="user.email"
+          />
+        </div>
 
-      <div class="form-group">
-        <label>Password</label>
-        <input
-          id="inp-login-password"
-          type="password"
-          class="form-control form-control-lg"
-          v-model="user.password"
-        />
-      </div>
+        <div class="form-group">
+          <label>Password</label>
+          <input
+            id="inp-login-password"
+            type="password"
+            class="form-control form-control-md input"
+            v-model="user.password"
+          />
+        </div>
 
-      <button type="submit" class="btn btn-dark btn-lg btn-block">
-        Sign In
-      </button>
+        <button type="submit" class="btn btn-success btn-lg btn-block">
+          Sign In
+        </button>
 
-      <p class="forgot-password text-right mt-2 mb-4">
-        <router-link to="/forgot-password">Forgot password ?</router-link>
-      </p>
-    </form>
+        <p class="forgot-password text-right mt-2 mb-4">
+          <router-link to="/forgot-password">Forgot password ?</router-link>
+        </p>
+      </form>
+    </card>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
+import Card from "@/components/Card.vue";
 
 export default {
+  components: { Card },
   data() {
     return {
       user: {
