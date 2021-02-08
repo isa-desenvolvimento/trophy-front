aa869e2c3ce397f222e1bf68671e359f045a35fd
Andressa Novaes <isa.menezes@ilia.digital>
2021-02-08 10:57:38 -0300

    fix: ajuste

--- src/service/auth.js
+++ src/service/auth.js
@@ -14,27 +14,23 @@ export const signOut = () => {
 };
 
 export const isSignedIn = async () => {
-  try {
-    const token = (await localStorage.getItem("token")) || null;
+  const token = await localStorage.getItem("token");
 
-    if (!token)
-      // Se não existe o token no LocalStorage
-      return false; // significa que o usuário não está assinado.
+  if (!token)
+    // Se não existe o token no LocalStorage
+    return false; // significa que o usuário não está assinado.
 
-    // try {
-    //   const { exp: expiration } = decode(token);
-    //   const isExpired = !!expiration && Date.now() > expiration * 1000;
+  // try {
+  //   const { exp: expiration } = decode(token);
+  //   const isExpired = !!expiration && Date.now() > expiration * 1000;
 
-    //   if (isExpired)
-    //     // Se o token tem uma data de expiração e
-    //     return false; // essa data é menor que a atual o usuário
-    //   // não está mais assinado.
-    return true;
-    // } catch (_) {
-    //   // O "jwt-decode" lança erros pra tokens inválidos.
-    //   return false; // Com um token inválido o usuário não está assinado.
-    // }
-  } catch (error) {
-    console.warn(error);
-  }
+  //   if (isExpired)
+  //     // Se o token tem uma data de expiração e
+  //     return false; // essa data é menor que a atual o usuário
+  //   // não está mais assinado.
+  return true;
+  // } catch (_) {
+  //   // O "jwt-decode" lança erros pra tokens inválidos.
+  //   return false; // Com um token inválido o usuário não está assinado.
+  // }
 };
